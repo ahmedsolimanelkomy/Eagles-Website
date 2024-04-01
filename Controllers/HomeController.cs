9c451e31beb7987d979e78139813c6665b9c8241
PoulaMaher <poula.maher199@gmail.com>
2024-04-01 03:18:49 +0200

    create custom Layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 using Eagles_Website.Models;
+using Eagles_Website.Repository;
+using Eagles_Website.Repository.IRepository;
+using Eagles_Website.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +10,18 @@ namespace Eagles_Website.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOFWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger , IUnitOFWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;   
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Product> products = _unitOfWork.ProductRepo.GetAll().ToList();
+            return View("Index" , products);
         }
 
         public IActionResult Privacy()
